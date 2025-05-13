d26460dfbb34e4d8baaf5ddb15183068ca561a18
luanagaloiu <luanagaloiu@gmail.com>
2025-05-14 00:18:40 +0200

    better arch with scalar features later

--- architecture.py
+++ architecture.py
@@ -9,67 +9,68 @@ class MyCNN(nn.Module):
     def __init__(self, num_features: int, num_classes=int):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(num_features, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(num_features, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        self.conv4 = nn.Sequential(
+        self.conv2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        self.conv5 = nn.Sequential(
+        self.conv3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        # self.conv6 = nn.Sequential(
-        #     nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(512),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(kernel_size=2, stride=2)
-        # )
-     
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Sequential(
-            #nn.Dropout(0.5),
-            nn.Linear(256*7*24, 512*3), 
-            nn.ReLU()
+        self.classifier = nn.Sequential(
+            nn.Linear(512+3*250, 512), 
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes), 
         )
-        self.fc2 = nn.Sequential(
-            #nn.Dropout(0.5),
-            nn.Linear(512*3, 2048), 
-            nn.ReLU()
-        )
-        self.fc3 = nn.Linear(2048, num_classes)
-        # self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, input_data):
+    def forward(self, input_data, scalar_features=None):
         x = self.conv1(input_data)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        x = self.conv5(x)
-        # x = self.conv6(x)
+        x = self.gap(x)
         x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        # x = self.softmax(x)
+        
+        if scalar_features is not None:
+            x = torch.cat([x] + scalar_features, dim=1)
+        
+        x = self.classifier(x)
         return x
     
 
--- main.py
+++ main.py
@@ -19,7 +19,14 @@ from sklearn.metrics import f1_score
 
 
 
-def evaluate_model(model: torch.nn.Module, loader: torch.utils.data.DataLoader, loss_fn, device: torch.device):
+def evaluate_model(
+    model: torch.nn.Module, 
+    loader: torch.utils.data.DataLoader, 
+    loss_fn, 
+    device: torch.device, 
+    primary_feature = "melspectrogram",
+    scalar_features = [ "centroid", "energy","zerocrossingrate"]
+):
     """Function for evaluation of a model ``model`` on the data in
     ``loader`` on device ``device``, using the specified ``loss_fn`` loss
     function."""
@@ -31,16 +38,20 @@ def evaluate_model(model: torch.nn.Module, loader: torch.utils.data.DataLoader, 
     macro_f1 = 0
     with torch.no_grad():  
         for data in tqdm(loader, desc="Evaluating", position=0, leave=False):
-            inputs, targets, _ = data
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            outputs = model(inputs)
-            loss += loss_fn(outputs, targets).item()
+            input = data[primary_feature]
+            input = input.to(device)
+            input_scalar = [data[feature] for feature in scalar_features]
+            input_scalar = [feature.to(device) for feature in input_scalar]
+            class_id = data['class_id']
+            class_id = class_id.to(device)
+
+            outputs = model(input, input_scalar)
+            loss += loss_fn(outputs, class_id).item()
             _, predicted = torch.max(outputs.data, 1)
-            total += targets.size(0)
-            correct += (predicted == targets).sum().item()
+            total += class_id.size(0)
+            correct += (predicted == class_id).sum().item()
             macro_f1 += f1_score(
-                y_true=targets.cpu().numpy(),
+                y_true=class_id.cpu().numpy(),
                 y_pred=torch.argmax(outputs, dim=1).cpu().numpy(),
                 average='macro'
             )
@@ -60,27 +71,23 @@ def main(
     num_epochs: int = 3, 
     train_batch_size: int = 16,
     momentum: float = 0.9,
-    test: bool = True,
-    early_stopping_threshold: int = 30,
-    selected_features: list[str] = None
-    
+    skip_test: bool = False,
+    early_stopping_threshold: int = 15,
+    primary_feature = "melspectrogram",
+    scalar_features = [ "centroid", "energy","zerocrossingrate"]
 ):
     print(f"Batch: {train_batch_size} | LR: {learning_rate:.0e} | WD: {weight_decay:.0e} | Momentum: {momentum}")
-    model = MyCNN(
-        num_features = 13 if selected_features is None else len(selected_features),  # Number of input channels
-        num_classes = 58  # Number of output classes
-    )
+    model = MyCNN(1, 58)
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
     np.random.seed(0)
     torch.manual_seed(0)
 
-    #dataset             
-    dataset = MLPC2025(selected_features=selected_features)
+    dataset = MLPC2025()
 
     #Split dataset into training and validation sets
-    training_size = int(len(dataset) * (3 / 5)) if test else int(len(dataset) * (4 / 5))
-    validation_size = int(len(dataset) * (4 / 5)) if test else len(dataset)
+    training_size = int(len(dataset) * (3 / 5)) if not skip_test else int(len(dataset) * (4 / 5))
+    validation_size = int(len(dataset) * (4 / 5)) if not skip_test else len(dataset)
     
     training_set = Subset(
         dataset,
@@ -91,7 +98,7 @@ def main(
         indices=np.arange(training_size, validation_size)
     )
 
-    if test:
+    if not skip_test:
         test_set = Subset(
             dataset,
             indices=np.arange(validation_size, len(dataset))
@@ -154,23 +161,24 @@ def main(
         model.train()
         
         for i, data in enumerate(training_loader):
-            features_tensor, classid, _ = data
+            input = data[primary_feature]
+            input = input.to(device)
+            input_scalar = [data[feature] for feature in scalar_features]
+            input_scalar = [feature.to(device) for feature in input_scalar]
+            class_id = data['class_id']
+            class_id = class_id.to(device)
             
-            
-
-            features_tensor = features_tensor.to(device)
-            classid = classid.to(device)
             optimizer.zero_grad()
             if torch.cuda.is_available():
                 with autocast():
-                    output = model(features_tensor)
-                    loss = loss_function(output, classid)
+                    output = model(input, input_scalar)
+                    loss = loss_function(output, class_id)
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
                 scaler.update()
             else:
-                output = model(features_tensor)
-                loss = loss_function(output, classid)
+                output = model(input, input_scalar)
+                loss = loss_function(output, class_id)
                 loss.backward()
                 optimizer.step()
             scheduler.step()
@@ -196,31 +204,32 @@ def main(
         writer.add_scalar(tag="Accuracy/Validation", scalar_value=val_acc, global_step=epoch)
         writer.add_scalar(tag="F1/Validation", scalar_value=macro_f1, global_step=epoch)
 
-        
-       
             
-        # if epoch - best_loss_epoch > early_stopping_threshold:
-        #     print("early stopping")
-        #     break #early stopping
+        if epoch - best_loss_epoch > early_stopping_threshold:
+            print("early stopping")
+            break
 
     print("Finished Training!")
     update_progress_bar.close()
     writer.close()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+    if torch.backends.mps.is_available():
+        torch.mps.empty_cache()
     gc.collect()
 
     print(f"Computing scores for best model")
     model.load_state_dict(torch.load(saved_model_file))
-    train_loss, train_acc, macro_f1 = evaluate_model(model, training_loader, loss_function, device)
-    val_loss, val_acc, macro_f1 = evaluate_model(model, validation_loader, loss_function, device)
-    if test:
-        test_loss, test_acc, macro_f1 = evaluate_model(model, test_loader, loss_function, device)
+    train_loss, train_acc, train_macro_f1 = evaluate_model(model, training_loader, loss_function, device)
+    val_loss, val_acc, val_macro_f1 = evaluate_model(model, validation_loader, loss_function, device)
+    if not skip_test:
+        test_loss, test_acc, test_macro_f1 = evaluate_model(model, test_loader, loss_function, device)
     
     print(f"Scores:")
-    print(f"  training loss / accuracy: {train_loss} / {train_acc * 100}%")
-    print(f"  validation loss / accuracy: {val_loss} / {val_acc * 100}%")
-    print(f"  Macro F1: {macro_f1}")
-    if test:
-        print(f"  test loss / accuracy: {test_loss} / {test_acc * 100}%\n")
+    print(f"  f1 / training loss / accuracy: {train_macro_f1} / {train_loss} / {train_acc * 100}%")
+    print(f"  f1 / validation loss / accuracy: {val_macro_f1} / {val_loss} / {val_acc * 100}%")
+    if not skip_test:
+        print(f"  f1 / test loss / accuracy: {test_macro_f1} / {test_loss} / {test_acc * 100}%\n")
         os.rename(saved_model_file, saved_model_file.replace("model", f"Acc_{test_acc:1.4f}_Loss_{test_loss:1.4f}_E{best_loss_epoch}-{num_epochs}"))
 
 
@@ -231,7 +240,8 @@ if __name__ == "__main__":
         learning_rate=1e-3, 
         weight_decay=1e-2, 
         momentum=.99, 
-        selected_features =['zerocrossingrate', 'centroid', 'embeddings'] 
+        primary_feature = "melspectrogram",
+        scalar_features = [ "centroid", "energy","zerocrossingrate"]
     )
 
     # for bs in [128, 96]:
